dbb3a30d145008805c8a82697be9781dd4459cfc
MassiBaldi <massibaldi85@gmail.com>
2018-11-27 19:29:42 +0100

    finito funzionante

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-$('.hamburger').click(function(){
-  $('.ham_menu').toggle()
-});
-
-//Funziona.... Avrei voluto fare altro Ma sono perso, volevo far andare via .hamburger e far comparire .x, cliccando poi su .x succedeva l'inverso, ovvero andava via la .x e tornava .hamburger. ho fatto prove arrivo ad un punto e la .x non appare
-
-
 // $('.hamburger').click(function(){
-//   $('.ham_menu').show(function(){
-//     $('.hamburger').hide(function(){
-//       $('.x').show(function(){
-//
-//       })
-//     })
-//   })
+//   $('.ham_menu').toggle()
 // });
+
+$('.hamburger').click(function(){
+  $('.ham_menu').show(function(){
+    $('.hamburger').hide(function(){
+    })
+  })
+});
+$('.x').click(function(){
+  $('.ham_menu').hide(function(){
+    $('.hamburger').show()
+  })
+});
